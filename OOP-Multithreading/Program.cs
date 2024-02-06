217470fe9ed41c0c61a6fb72d1e5f3b247cfe4a7
janthony25 <junanthony.business@gmail.com>
2024-02-06 16:07:31 +1300

    Add project files.

---
+++ OOP-Multithreading/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // thread = an execution path of a program
+            //          we can use multiple threads to perform,
+            //          different tasks of our program at the same time.
+            //          Current thread running is "main" thread
+            //          using System.Threading;
+
+            Thread mainThread = Thread.CurrentThread;
+
+            mainThread.Name = "Main Thread";
+            //Console.WriteLine(mainThread.Name);
+
+            Thread thread1 = new Thread(CountDown);
+            Thread thread2 = new Thread(CountUp);
+            thread1.Start();
+            thread2.Start();
+
+
+            //Console.WriteLine(mainThread.Name = " is complete!");
+
+            Console.ReadKey();
+        }
+
+        public static void CountDown()
+        {
+            for(int i = 10; i >= 0 ; i--)
+            {
+                Console.WriteLine($"Timer #1 {i} seconds");
+                Thread.Sleep(1000);
+            }
+        }
+
+        public static void CountUp()
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine($"Timer #2 {i} seconds");
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
